7c9e9c077ccb5c90beaf27b56d947f35429bca46
John Luke <joliveira@mwneo.com>
2022-11-16 00:13:52 -0300

    fix: cpf validation

--- school/serializer.py
+++ school/serializer.py
@@ -1,10 +1,24 @@
 from rest_framework import serializers
-from school.models import Student, Course, Registration
+from validate_docbr import CPF
+
+from school.models import Course, Registration, Student
+
+cpf_lib = CPF()
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'
+
+    def validate_cpf(self, cpf):
+        if not cpf_lib.validate(cpf):
+            raise serializers.ValidationError("Invalid CPF")
+        return cpf
+    
+    def validate_name(self, name):
+        if not name.replace(" ", "").isalpha():
+            raise serializers.ValidationError("The name must not contain special characters")
+        return name
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,3 +39,9 @@ class ListRegisterStudentSerializer(serializers.ModelSerializer):
 
     def get_period(self, obj):
         return obj.get_period_display()
+
+class ListStudentRegisteredPerCourseSerializer(serializers.ModelSerializer):
+    student_name = serializers.ReadOnlyField(source='student.name')
+    class Meta:
+        model = Registration
+        fields = ['student_name']
