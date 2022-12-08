d1beaf45889fd4a2a46ca856a4ddd0c12eeedd7b
João Lucas <62743404+joaoluke@users.noreply.github.com>
2022-12-08 03:18:30 -0300

    fix: add localhost to host allowed

--- setup/settings.py
+++ setup/settings.py
@@ -118,8 +118,10 @@ USE_I18N = True
 USE_TZ = True
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'http://localhost:5173'
 ]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
